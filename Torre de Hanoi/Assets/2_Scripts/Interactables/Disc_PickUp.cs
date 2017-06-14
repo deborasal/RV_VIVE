b877a9252b0f76f3deb15e436c6a74e4ff9dcee5
Debora Salgado <deborapsalgado@gmail.com>
2017-06-14 01:03:58 -0300

    changed scales and added new scrip at manage layers and disco_1

--- Torre de Hanoi/Assets/2_Scripts/Interactables/Disc_PickUp.cs
+++ Torre de Hanoi/Assets/2_Scripts/Interactables/Disc_PickUp.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Disc_PickUp : Pickup_Touch {
 
+    public Transform manager;
     private Vector3 initialPos;
 
     protected override void Start()
@@ -16,5 +17,28 @@ public class Disc_PickUp : Pickup_Touch {
     {
         base.OnManipulationEnded(caller);
         //Verificar se está na barra: Cair se sim, voltar à posição inicial se não
+    }
+
+    public override bool CanBeManipulated(Transform other)
+    {
+        initialPos = transform.position;
+        return base.CanBeManipulated(other) && GetInteractionDistance(other) < grabRange && manager.GetComponent<disk_manage>().TryPop(transform.GetComponent<properties>().GetSize(), initialPos);
+    }
+
+    protected override void GetDropped(Vector3 throwVelocity)
+    {
+        if (!manager.GetComponent<disk_manage>().TryPush(transform.GetComponent<properties>().GetSize(), transform.position))
+        {
+            transform.position = initialPos;
+            manager.GetComponent<disk_manage>().TryPush(transform.GetComponent<properties>().GetSize(), transform.position);
+            manager.GetComponent<disk_manage>().BackToInitialPos();
+        }
+
+        holder.SetManipulatedInteractable(null);
+
+        rby.useGravity = true;
+        rby.isKinematic = false;
+
+        rby.velocity = throwVelocity;
     }
 }
