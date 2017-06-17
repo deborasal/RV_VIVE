dd8f21e5ef08ba38f5437997604547a57d024daf
Debora Salgado <deborapsalgado@gmail.com>
2017-06-17 13:34:18 -0300

    Added the hanoi program with mouse coursor

---
+++ Torre de Hanoi_Mouse/TF-Torre de Hanoi/Torre de Hanoi/Assets/Scripts/moveCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCursor : MonoBehaviour
+{
+
+    private static float offsetFromPlayer = 1f;
+
+    public Transform player;
+    public Transform mousePosMarker;
+
+    private Vector3 aux_position;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        aux_position = player.position;
+        aux_position.z += offsetFromPlayer;
+        mousePosMarker.position = aux_position;
+	}
+}
